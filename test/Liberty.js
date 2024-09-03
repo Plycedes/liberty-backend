bb8b1eb05c2c264cb36a2dd9d5397edb2a4eb74a
Plycedes <abhaypratapyadav340@gmail.com>
2024-09-03 13:38:07 +0530

    Tests

--- test/Liberty.js
+++ test/Liberty.js
@@ -13,13 +13,26 @@ describe("Liberty", async () => {
     });
 
     describe("createPetition", async () => {
-        it("creates a petiton", async () => {
+        it("Creates a petiton", async () => {
             const initalNum = Number(await liberty.numberOfPetitions());
             const tx = await liberty.createPetition(owner, title, description, image);
             const tr = await tx.wait();
-            console.log(tr);
-            const finalNum = Number(await liberty.numberOfPetitions());
+            //console.log(tr.logs[0].args[0]);
+            const finalNum = Number(tr.logs[0].args[0]);
             assert.equal(initalNum + 1, finalNum);
+        });
+    });
+
+    describe("voteToPetition", async () => {
+        beforeEach(async () => {
+            const tx = await liberty.createPetition(owner, title, description, image);
+            await tx.wait();
+        });
+        it("Casts vote", async () => {
+            const tx = await liberty.voteToPetition(1);
+            const tr = await tx.wait();
+            const votes = Number(tr.logs[0].args[0]);
+            assert.equal(1, votes);
         });
     });
 });
